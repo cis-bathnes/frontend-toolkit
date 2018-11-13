00945369a48999249222b281ecd05e6d0a6e7f15
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2018-11-13 09:41:59 +0000

    updated CT Directory

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -1,3 +1,12 @@
 /**
  * Toolkit JavaScript
  */
+
+
+function ifradioCheck() {
+  if (document.getElementById('where-do-you-live-1').checked) {
+      document.getElementById('bath-ct').style.display = 'block';
+  }
+  else document.getElementById('bath-ct').style.display = 'none';
+
+}
